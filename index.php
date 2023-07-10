37b292e77093f6c3b91768742630c5f348b70225
Kevin Barreto <barretokevin@hotmail.com>
2023-07-10 08:22:36 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Main template file
+ * 
+ * @package Aquila
+ */
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=2.0">
+    <title>Aquila WordPress</title>
+</head>
+
+<body>
+    <h1>hello wordpress</h1>
+</body>
+
+</html>
